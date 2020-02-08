2875a47125dc490a29e96495f2a63ce4ef737a39
admin <rakdav@inbox.ru>
2020-02-08 17:16:09 +0200

    ce

--- app/src/main/java/com/example/unbindservice/MainActivity.java
+++ app/src/main/java/com/example/unbindservice/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.unbindservice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button start;
+    private Button stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start=findViewById(R.id.button_play);
+        stop=findViewById(R.id.button_stop);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,PlaySongService.class);
+                startService(intent);
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,PlaySongService.class);
+                stopService(intent);
+            }
+        });
     }
 }
